148f8d96873494ced9696279dde1f8e92e0ff589
hiyoridesu <160451772+hiyoridesu@users.noreply.github.com>
2024-02-20 16:37:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask,request,render_template
 app = Flask(__name__)
 
+r = " "
+first_time=1
+
 @app.route("/",methods=["GET","POST"])
 def index():
   return (render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-  r = request.form.get("r")
+  if first_time = 1:
+    r = request.form.get("r")
+    r = request.form.get("r")
+    first_time=0
   return (render_template("main.html",r = r))
 
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
   return(render_template("/image_gpt.html"))
 
+@app.route("/end",methods=["GET","POST"])
+def end():
+  globle first_time = 1
+  return(render_template("/image_gpt.html"))
 
 if __name__ == "__main__":
   app.run()
