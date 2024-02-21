21a9ffbc733d5dc22c4255b865976c36f79119f5
hiyoridesu <160451772+hiyoridesu@users.noreply.github.com>
2024-02-21 11:27:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def image_result():
   r = replicate.run(
    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
 
-    input={"prompt": inputs,}
+    input={"prompt": q,}
   )
   time.sleep(10)
   return(render_template("image_gpt.html",r = r[0]))
